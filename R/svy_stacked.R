a056ec222e30da19726fa2069b48232a171d95a7
Stephanie Zimmer <stephaniezimmer@gmail.com>
2025-12-22 20:30:16 -0500

    Streamline assertions

--- R/svy_stacked.R
+++ R/svy_stacked.R
@@ -4,14 +4,23 @@
 #' For example, comparing a state estimate to the national or all adults to employed adults.
 #'
 #' @param data a data frame or tibble to look up formulas and necessary variables for design objects
-#' @param svydestype a character string indicating the type of survey design object to create. Options are "svydesign" or "svrepdesign".
-#' @param svydesargs a list of arguments to pass to the survey design constructor function [survey::svydesign()] or [survey::svrepdesign()]) - do NOT include the data.
-#' @param outcome_variable an outcome variable, specified as a character, which is what the test is comparing
-#' @param subpop_variable a variable, specified as character, which defines the subpopulation(s) comparing to the full population
-#' @param subset_statement (optional) any necessary subsetting to do before the analysis, specified as a character (e.g., "mpg > 4")
-#' @param dftype specify what type of degrees of freedom to use either specified as one of c("entire", "full", "sub") or a specified positive number where "entire" uses the degrees of freedom for the entire design, "full" uses the degrees of freedom for the design after applying the subset, this will be the same as "entire" if there is no subset, or "sub" which uses the degrees of freedom after applying both the subset and the subpopulation. Otherwise, if a number is specified, that is used as the degrees of freedom in all tests.
+#' @param svydestype a character string indicating the type of survey design object to create.
+#' Options are "svydesign" or "svrepdesign".
+#' @param svydesargs a list of arguments to pass to the survey design constructor function
+#" [survey::svydesign()] or [survey::svrepdesign()]) - do NOT include the data.
+#' @param outcome_variable an outcome variable, specified as a character
+#' @param subpop_variable a variable, specified as character, which defines the subpopulation(s)
+#' comparing to the full population
+#' @param subset_statement (optional) any necessary subsetting to do before the analysis, specified
+#' as a character (e.g., "mpg > 4")
+#' @param dftype specify what type of degrees of freedom to use either specified as one of
+#' c("entire", "full", "sub", "manual") or a specified positive number where "entire" uses the
+#' degrees of freedom for the entire design, "full" uses the degrees of freedom for the design after
+#' applying the subset, this will be the same as "entire" if there is no subset, "sub" which uses
+#' the degrees of freedom after applying both the subset and the subpopulation or "manual" where the
+#' degrees of freedom is specified in ddf
+#' @param ddf a positive value for degrees of freedom if using manual degrees of freedom
 #' @examples
-#'
 #' data(api, package="survey")
 #' svy_stacked(data = apistrat,
 #'             svydestype = "svydesign",
@@ -35,36 +44,29 @@
 #'   )
 #' }
 #' @export
-svy_stacked <- function(data, svydestype = c("svydesign", "svrepdesign"), svydesargs, outcome_variable, subpop_variable, subset_statement = NULL, dftype = "sub") {
-  if (!is.data.frame(data)) {
-    stop("data must be a data frame or tibble")
-  }
-  if (!(svydestype %in% c("as_survey_design", "as_survey_rep", "svydesign", "svrepdesign"))) {
-    stop('svydestype must be either "svydesign" or "svrepdesign"')
-  }
-  if (!is.list(svydesargs) && is.data.frame(svydesargs)) {
-    stop("svydesargs must be a list")
-  }
+svy_stacked <- function(
+    data, svydestype = c("svydesign", "svrepdesign"), svydesargs, outcome_variable, subpop_variable,
+    subset_statement = NULL, dftype = "sub", ddf = NULL) {
+  # Validate inputs
+  checkmate::expect_data_frame(data)
+  checkmate::expect_choice(svydestype, c("svydesign", "svrepdesign"))
+  checkmate::expect_list(svydesargs)
   if ("data" %in% names(svydesargs)) {
-    stop("data must NOT be included in svydesargs, it is passed as a separate object")
+    stop("'data' must NOT be included in 'svydesargs' because it is passed as a separate object")
   }
-  if (!is.character(outcome_variable) || length(outcome_variable) != 1 || !outcome_variable %in% names(data)) {
-    stop("outcome variable must be ONE variable on the input data")
-  }
-  if (!is.character(subpop_variable) || length(subpop_variable) != 1 || !subpop_variable %in% names(data)) {
-    stop("subpop variable must be ONE variable on the input data")
-  }
-  if (!is.null(subset_statement)) {
-    if (!is.character(subset_statement) || length(subset_statement) != 1) {
-      stop("subset_statement must be a character variable")
-    }
+  checkmate::expect_character(outcome_variable, len=1, any.missing = FALSE)
+  checkmate::expect_subset(outcome_variable, names(data))
+  checkmate::expect_character(subpop_variable, len=1, any.missing = FALSE)
+  checkmate::expect_subset(subpop_variable, names(data))
+  checkmate::expect_character(subset_statement, len=1, any.missing = FALSE, null.ok = TRUE)
+  checkmate::expect_choice(dftype, c("entire", "full", "sub", "manual"))
+  if (dftype=="manual"){
+    checkmate::expect_number(ddf, lower=.000000001)
+  } else{
+    testthat::expect_null(ddf)
   }
 
-  ok_df_char <- is.character(dftype) && length(dftype) == 1 && dftype %in% c("entire", "full", "sub")
-  ok_df_num <- is.numeric(dftype) && length(dftype) == 1 && dftype > 0
-  if (!(ok_df_char | ok_df_num)) {
-    stop("dftpe must either be one of 'entire', 'full', or 'sub' or a positive number")
-  }
+  # Stack data and create a group variable
 
   groupvar <- make.names(c(names(data), ".group"), unique=TRUE)[ncol(data)+1]
 
@@ -74,6 +76,8 @@ svy_stacked <- function(data, svydestype = c("svydesign", "svrepdesign"), svydes
 
   svydesargs$data <- rbind(data_full, data_sub)
 
+  # Crate survey design object
+
   if (svydestype == "svydesign" && !any(c("ids", "id") %in% names(svydesargs))) {
     svydesargs[["ids"]] <- ~1
     message("ids set to ~1 for design as none is provided.")
@@ -81,14 +85,15 @@ svy_stacked <- function(data, svydestype = c("svydesign", "svrepdesign"), svydes
 
   stacked_des <-
     switch(svydestype,
-      # as_survey_design = do.call(srvyr::as_survey_design, svydesargs),
-      # as_survey_rep = do.call(srvyr::as_survey_rep, svydesargs),
-      svydesign = do.call(survey::svydesign, svydesargs) |> srvyr::as_survey(),
-      svrepdesign = do.call(survey::svrepdesign, svydesargs) |> srvyr::as_survey()
+           # as_survey_design = do.call(srvyr::as_survey_design, svydesargs),
+           # as_survey_rep = do.call(srvyr::as_survey_rep, svydesargs),
+           svydesign = do.call(survey::svydesign, svydesargs) |> srvyr::as_survey(),
+           svrepdesign = do.call(survey::svrepdesign, svydesargs) |> srvyr::as_survey()
     )
 
 
   # Subset design if subset condition provided
+
   if (!is.null(subset_statement)) {
     subset_quote <- rlang::parse_expr(subset_statement)
     subset_des <- stacked_des |> srvyr::filter(!!subset_quote)
@@ -96,15 +101,14 @@ svy_stacked <- function(data, svydestype = c("svydesign", "svrepdesign"), svydes
     subset_des <- stacked_des
   }
 
-
   if (dftype == "entire") {
     dfuse <- survey::degf(stacked_des)
   } else if (dftype == "full") {
     dfuse <- survey::degf(subset_des)
   } else if (dftype == "sub") {
     dfuse <- NA
-  } else {
-    dfuse <- dftype
+  } else if (dftype == "manual"){
+    dfuse <- ddf
   }
 
   subtest <- function(subpopval) {
